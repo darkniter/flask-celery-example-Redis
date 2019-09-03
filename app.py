ca25d1909e2ca830abe40fd119a8a11109c356b0
darkniter <Poranot@yandex.ru>
2019-09-03 13:16:59 +0300

    fix compose and connection on redis

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ app.config['MAIL_DEFAULT_SENDER'] = 'flask@example.com'
 mail = Mail(app)
 
 # Initialize Celery
-celery = Celery(app.name,broker='amqp://redis:6380/13',
-             backend='amqp://redis:6380/13',)
+celery = Celery(app.name,broker='redis://redis:6379/13',
+             backend='redis://redis:6379/13',)
 celery.conf.update(app.config)
 
 
